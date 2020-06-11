581ae2a43d145dd7f64817aa50588d3a73ad111b
ashrawan <shrawan.adhikari@logicabeans.com>
2020-06-11 16:38:05 +0545

    main page  layout and content updated

--- src/components/container/Header.js
+++ src/components/container/Header.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ContactCircle from './ContactCircle';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ContactCircle from "./ContactCircle";
 
 const Header = () => {
     return (
-        <header className="masthead text-center text-white d-flex">
-            <div className="container my-auto">
-                <div className="row">
-                    <div className="col-lg-10 mx-auto">
-                        <h1 className="text-uppercase">
-                            <strong>- Shrawan Adhikari -</strong>
-                        </h1>
-                        <br />
+        <header className="masthead bg-primary text-white text-center">
+            <div className="container d-flex align-items-center flex-column">
+                <h1 className="masthead-heading mb-0">Shrawan Adhikari</h1>
+                <div className="divider-custom divider-light">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon">
+                        <FontAwesomeIcon icon={["fas", "code"]} size="md" />
                     </div>
-                    <div className="col-lg-8 mx-auto">
-                        <p className="text-faded mb-5">Software Engineer with professional Industry experience on designing and developing core back-end applications using Java and Spring; and explores and loves both Angular, and React.</p>
-                        <ContactCircle />
-                    </div>
+                    <div className="divider-custom-line"></div>
                 </div>
+                <ContactCircle />
+                <p className="masthead-subheading">
+                    Software Engineer with valuable skills in System Design, Analysis and Development. Includes 2+ year of professional Industry experience
+                    with hands-on experience on core back-end technologies with Java, Spring, Hibernate and MySQL; and explores and loves both Angular, and React.
+                </p>
             </div>
         </header>
     );
